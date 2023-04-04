a81bf32884131dc058e92f63418fdfcd855199d2
jinalpatelPHPDev <jinalss@itidoltechnologies.com>
2023-04-04 12:06:52 +0530

    changes_jinal

--- app/Http/Controllers/API/InstagramApiController.php
+++ app/Http/Controllers/API/InstagramApiController.php
@@ -5,24 +5,26 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Iposts;
+use App\Models\instagram;
 
 
 class InstagramApiController extends Controller
 {
-    //
+    
     public function instagram()
     {
-        $products1 = Iposts::select()->orderBy('id','desc')->get();
-        // $products2 = Facebook::select()->orderBy('id','desc')->get();
+         
+    $Iposts =  Iposts::select('id', 'title', 'description', 'images')->get()->toArray();
+    foreach ($Iposts as $key => $value) {
+    $data['Iposts'][$key] = $value;
+        }
 
-
-        print_r($products1);exit();
-
-        return response()->json([
-        "success" => true,
-        "message" => "Instagram posts",
-        "data" => $products1
-        ]);
+     $instagram =  instagram::select('id', 'title', 'description', 'image','date','status')->get()->toArray();
+        foreach ($instagram as $key => $value) {
+         $data['instagram'][$key] = $value;
+        }
+       
+     return($data);
       
 
     }
