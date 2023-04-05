b87cbd4218ecf186290486d89c4f27e82c1fd977
jinalpatelPHPDev <jinalss@itidoltechnologies.com>
2023-04-05 16:11:36 +0530

    user_login changes

---
+++ database/migrations/2023_04_05_102116_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+           $table->id();
+            $table->string('namePrefix')->nullable();
+            $table->string('firstname')->nullable();;
+            $table->string('lastname')->nullable();;
+            $table->string('middlename')->nullable();;
+            $table->string('nickname')->nullable();
+            $table->string('nameSuffix')->nullable();
+            $table->string('email')->unique()->nullable();;
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('is_admin')->nullable();
+            $table->string('password');
+            $table->string('mobileno')->unique()->nullable();;
+            $table->string('otp')->nullable();;
+            $table->rememberToken();
+            $table->timestamps();
+            $table->string('google_id')->nullable();
+            $table->string('facebook_id')->nullable();
+            $table->string('instagram_id')->nullable();
+            $table->string('apple_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
